573ea51c88187b12be1581dcda9e3c488d35ac4f
firekesti <fire.kesti@gmail.com>
2011-02-16 00:34:40 -0500

    Uploaded Hello World extension files

---
+++ chrome/content/main.js
@@ -0,0 +1,111 @@
+
+// Make a namespace.
+if (typeof Bluejay == 'undefined') {
+  var Bluejay = {};
+}
+
+/**
+ * UI controller that is loaded into the main player window
+ */
+Bluejay.Controller = {
+
+  /**
+   * Called when the window finishes loading
+   */
+  onLoad: function() {
+
+    // initialization code
+    this._initialized = true;
+    this._strings = document.getElementById("bluejay-strings");
+    
+    // Perform extra actions the first time the extension is run
+    if (Application.prefs.get("extensions.bluejay.firstrun").value) {
+      Application.prefs.setValue("extensions.bluejay.firstrun", false);
+      this._firstRunSetup();
+    }
+
+
+    // Add the toolbar button to the default item set of the browser toolbar.
+    // TODO: Should only do this on first run, but Bug 6778 requires doing it
+    // every load.
+    this._insertToolbarItem("nav-bar", "bluejay-toolbarbutton", "subscription-button");
+
+    
+
+    // Make a local variable for this controller so that
+    // it is easy to access from closures.
+    var controller = this;
+    
+    // Attach doHelloWorld to our helloworld command
+    this._helloWorldCmd = document.getElementById("bluejay-helloworld-cmd");
+    this._helloWorldCmd.addEventListener("command", 
+         function() { controller.doHelloWorld(); }, false);
+
+  },
+  
+
+  /**
+   * Called when the window is about to close
+   */
+  onUnLoad: function() {
+    this._initialized = false;
+  },
+  
+
+  /**
+   * Sample command action
+   */
+  doHelloWorld : function() {
+    var message = "Bluejay: " + this._strings.getString("helloMessage");
+    alert(message);
+  },
+
+  
+  /**
+   * Perform extra setup the first time the extension is run
+   */
+  _firstRunSetup : function() {
+  
+    // Call this.doHelloWorld() after a 3 second timeout
+    setTimeout(function(controller) { controller.doHelloWorld(); }, 3000, this); 
+  
+  },
+  
+  
+
+  /**
+   * Helper to add a toolbaritem within a given toolbar
+   * 
+   *   toolbar - the ID of a toolbar element
+   *   newItem - the ID of a toolbaritem element within the 
+   *            associated toolbarpalette
+   *   insertAfter - ID of an toolbaritem after which newItem should appear
+   */
+  _insertToolbarItem: function(toolbar, newItem, insertAfter) {
+    var toolbar = document.getElementById(toolbar);
+    var list = toolbar.currentSet || "";
+    list = list.split(",");
+    
+    // If this item is not already in the current set, add it
+    if (list.indexOf(newItem) == -1)
+    {
+      // Add to the array, then recombine
+      insertAfter = list.indexOf(insertAfter);
+      if (insertAfter == -1) {
+        list.push(newItem);
+      } else {
+        list.splice(insertAfter + 1, 0, newItem);
+      }
+      list = list.join(",");
+      
+      toolbar.setAttribute("currentset", list);
+      toolbar.currentSet = list;
+      document.persist(toolbar.id, "currentset");
+    }
+  }
+
+  
+};
+
+window.addEventListener("load", function(e) { Bluejay.Controller.onLoad(e); }, false);
+window.addEventListener("unload", function(e) { Bluejay.Controller.onUnLoad(e); }, false);
---
+++ defaults/preferences/prefs.js
@@ -0,0 +1,11 @@
+
+pref("extensions.bluejay.boolpref", false);
+pref("extensions.bluejay.intpref", 0);
+pref("extensions.bluejay.stringpref", "A string");
+
+
+// Used to detect the first time the extension is run 
+pref("extensions.bluejay.firstrun", true);
+
+// See http://kb.mozillazine.org/Localize_extension_descriptions
+// pref("extensions.{15988966-199b-421f-beb5-ddb8f3f565b1}.description", "chrome://bluejay/locale/bluejay.properties");
