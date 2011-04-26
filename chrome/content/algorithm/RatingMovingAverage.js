1a04d0f87d47b2103877bb45de6c53cd567c1b9a
sytianhe <sytianhe@gmail.com>
2011-04-26 15:46:21 -0400

    update RatingMovingAverage.js -Tian

--- chrome/content/algorithm/RatingMovingAverage.js
+++ chrome/content/algorithm/RatingMovingAverage.js
@@ -6,6 +6,7 @@
 	//public functions
 	this.addRating = addRating;
 	this.getValueAt = getValueAt;
+	this.getRatings = getRatings;
 	this.getNumRatings = getNumRatings;
 	this.getAverageValue = getAverageValue;
 	this.getLatestDate = getLatestDate;
@@ -25,7 +26,7 @@
 		var totalSquaredScore = 0.0;
 		
 		if(sumRatings.length > 0){
-			var lastRating = sumRatings[sumRatings.length];
+			var lastRating = sumRatings[sumRatings.length-1];
 			totalScore = lastRating.getScore();
 			totalWeight = lastRating.getWeight();
 			var sumSquaredRating = sumSquaredRatings[sumSquaredRatings];
@@ -58,5 +59,123 @@
 		ratings.push(rating);
 	}
 			
+	function getValueAt(when, strictlyEarlier){
+		
+		if (sumRatings.length == 0){
+			return [Distribution(0, 0, 0), -1];
+		}
+		
+		var firstRating = sumRatings[0];
+		if(!strictlyChronologicallyOrdered(firstRating(firstRating.getDate(), when)){
+			return [Distribution(0, 0, 0), -1];
+		}
+		
+		var lastestRatingIndex = getIndexForDate(when, strictlyEarlier);
+		var latestRatingDate = sumRatings[latestRatingIndex].getDate();
+		var duration = latestRatingDate.timeUntil(when);
+		var oldestRatingDate = latestRatingDate.datePlusDuration(-duration);
+		var earliestRatingIndex = getIndexForDate(oldestRatingDate, true);
+		
+		var latestSumRating = sumRatings[latestRatingIndex];
+		var latestSumSquaredRating = sumSquaredRatings[latestRatingIndex];
+		var earliestSumRating = sumRatings[earliestRatingIndex];
+		var earliestSumSquaredRating = sumSquaredRatings[earliestRatingIndex];
+		
+		var sumY = 0.0;
+		var sumY2 = 0.0;
+		var sumWeight = 0.0;
+		
+		if(strictlyChronologicallyOrdered(oldestRatingDate, sumRatings[0].getDate())){
+			sumY = latestSumRating.getScore();
+			sumY2 = latestSumSquaredRating.getScore();
+			sumWeight = latestSumRating.getWeight();
+		}
+		else{
+			sumY = latestSumRating.getScore() - earliestSumRating.getScore();
+			sumY2 = latestSumSquaredRating.getScore() - earliestSumSquaredRating.getScore();
+			sumWeight = latestSumSquaredRating.getWeight() - earliestSumSquaredRating.getWeight();
+		}
+		
+		var average = sumY/sumWeight;
+		var stdDev = Math.sqrt((sumY2 - sumY * sumY/sumWeight)/sumWeight);
+		var result = Distribution(average, stdDev, sumWeight);
+		
+		return [result, latestRatingIndex];
+	}
 	
+	function getRatings(){
+		return ratings;
+	}
 	
+	function getNumRatings(){
+		return sumRatings.length;
+	}
+	
+	function getIndexForDate(when, strictlyEarlier){
+		if(sumRatings.length < 1){
+			return -1;
+		}
+		
+		if(strictlyChronologicallyOrdered(sumRatings[sumRatings.length -1].getDate(), when)){
+			return sumRatings.length - 1;
+		}
+		
+		var lowerIndex = 0;
+		var upperIndex = 0;
+		var middleIndex = 0;
+		
+		upperIndex = sumRating.length - 1;
+		
+		while (upperIndex > lowerIndex + 1)
+		{
+			middleIndex = (lowerIndex + upperIndex) / 2;
+			if (strictlyChronologicallyOrdered(sumRatings[middleIndex].getDate(), when)){
+				lowerIndex = middleIndex;
+			}
+			else{
+				upperIndex = middleIndex;
+			}
+		}
+		
+		var previousRating = sumRatings[lowerIndex];
+		var nextRating = sumRatings[upperIndex];
+		
+		if (strictlyEarlier){
+			return lowerIndex;
+		}
+		
+		else
+		{
+			if(strictlyChronologicallyOrdered(when, nextRating.getDate())){
+				return lowerIndex;
+			}
+			else{
+				return upperIndex;
+			}
+		}
+	}
+	
+	function getAverageValue(){
+		if(sumRatings.length < 1){
+			return 0;
+		}
+		
+		return sumRatings[sumRatings.length -1].getScore()/(sumRatings.length);
+	}
+	
+	function getLatestDate()
+	{
+		if(sumRatings.length < 1){
+			return DateTime();
+		}
+		else{
+			return sumRatings[sumRatings.length -1].getDate);
+		}
+	}
+}
+		
+		
+		
+		
+		
+		
