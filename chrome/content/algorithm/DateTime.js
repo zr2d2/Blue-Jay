983bc0cdf504350d90faa9be8ac8d6d187cef6c8
Zach Rowe <rowez@rpi.edu>
2011-04-26 20:13:46 -0400

    changed DateTime to allow for initialization

--- chrome/content/algorithm/DateTime.js
+++ chrome/content/algorithm/DateTime.js
@@ -1,7 +1,7 @@
 /**
- *  DateTime object
- */
- 
+*  DateTime object
+*/
+
 function DateTime(yyyymmddhhmmss) {
     //public function prototypes
     this.datePlusDuration= datePlusDuration;
@@ -9,15 +9,16 @@ function DateTime(yyyymmddhhmmss) {
     this.timeUntil = timeUntil;
 
     //private variables 
-    var value;
+    var value = new Date();
+    setComponents(yyyymmddhhmmss);
 
     //public functions
     //create a new date equal to this date plus numSeconds
     function datePlusDuration(numSeconds) {
-        var result;
-        result = value + numSeconds;
-        if (timeUntil(result) != numSeconds){
-            document.write("date addition error");
+        var duration = new Date(numSeconds*1000);
+        var result = new Date(value.getTime() + numSeconds*1000);
+        if (timeUntil(result) != numSeconds*1000){
+            alert(timeUntil(result).valueOf() + " not equal to " + numSeconds + " " + newTime );
         }
         return result;
     }
@@ -29,24 +30,34 @@ function DateTime(yyyymmddhhmmss) {
 
     // the number of seconds from this to other
     function timeUntil(other) {
-        var result;
-        result = other - this.value;
+        var result = new Date();
+        result = other.valueOf() - value.valueOf();
         return result;
     }
 
     //private functions
 
     // assign the given datestring to this date
-    // An example datestring would be "1970-01-01T01:01:01"
+    // An example datestring would be "1970 00 01 01 01 01"
     function setComponents(yyyymmddhhmmss) {
-        value = yyyymmddhhmmss | "1970-01-01T01:01:01";
-    }
-
-    // returns true if t1 comes before t2, and false if t2 is equal to or after t1
-    function strictlyChronologicallyOrdered(t1, t2){
-        if (t1.timeUntil(t2)>0){
-            return true;
+        if(!yyyymmddhhmmss){
+            yyyymmddhhmmss = "1970 00 01 00 00 00";
         }
-        return false;
+        var newTime = yyyymmddhhmmss.split(" ");
+        value.setYear(newTime[0]);
+        value.setMonth(newTime[1]);
+        value.setDate(newTime[2]);
+        value.setHours(newTime[3]);
+        value.setMinutes(newTime[4]);
+        value.setSeconds(newTime[5]);
     }
 };
+
+// returns true if t1 comes before t2, and false if t2 is equal to or after t1
+function strictlyChronologicallyOrdered(t1, t2){
+    if (t1.timeUntil(t2)>0){
+        return true;
+    }
+    return false;
+}
+
