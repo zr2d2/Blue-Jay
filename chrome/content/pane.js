11c63f838f02e35dbcaf8d516a9495f0998e8ae4
Jeff <gastoj3@rpi.edu>
2011-05-01 02:14:32 -0400

    fixed a really annoying tendency for the engine to play a song that was just recently skipped

--- chrome/content/pane.js
+++ chrome/content/pane.js
@@ -120,8 +120,8 @@ Bluejay.PaneController = {
     alert("push [ok] to start scanning library");
     var length = list.length;
     // limit the number of songs (for testing)
-    if (length > 50)
-        length = 50;
+    if (length > 65)
+        length = 65;
     var music = new Name("Song");
     for (i = 0; i < length; i++){
         var songName = new Name(list.getItemByIndex(i).getProperty(SBProperties.trackName));
